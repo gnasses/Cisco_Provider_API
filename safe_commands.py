5fef33ca79b9ef5708f2f00b6b1c6283e7df5cc4
Nasses, George W <george.nasses@optum.com>
2020-11-07 13:36:23 -0600

    Create safe_commands.py

---
+++ safe_commands.py
@@ -0,0 +1,9 @@
+valid = ['show version', 
+'show inventory', 
+'show hardware', 
+'show cdp neighbor', 
+'show cdp neighbor detail', 
+'show ip route', 
+'show ip route vrf all', 
+'show bgp summary']
+
