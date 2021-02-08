ca8f12757c314b11614adc5912d70094b45edae8
Nasses, George W <george.nasses@optum.com>
2021-02-08 17:00:27 -0600

    Create esm.py

---
+++ esm.py
@@ -0,0 +1,3 @@
+url = "https://esm.uhc.com/AIMWebService/api/Accounts"
+querystring = {"AppID":"APPID-001-CU9NSSFJU-P","Safe":"ESM-001-CU9NSSFJU-P-01","Folder":"Root","Object":"Application-ESM-001-NOMANAGE-STANDARD-nsdv_dev"}
+headers = {'content-type': 'application/json'}
