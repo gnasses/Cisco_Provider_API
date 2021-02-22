8ddbb5161ac8ca0397c56dc487c1723b0d5be77e
Gus Nasses <35275315+gnasses@users.noreply.github.com>
2021-02-22 11:59:38 -0600

    Update esm.py

--- esm.py
+++ esm.py
@@ -1,3 +1,3 @@
-url = "https://esm.uhc.com/AIMWebService/api/Accounts"
-querystring = {"AppID":"APPID-001-CU9NSSFJU-P","Safe":"ESM-001-CU9NSSFJU-P-01","Folder":"Root","Object":"Application-ESM-001-NOMANAGE-STANDARD-nsdv_dev"}
+url = "https://MyCyberArkServer/AIMWebService/api/Accounts"
+querystring = {"AppID":"APPID-001-CU9NSSFJU-P","Safe":"<safe_object>","Folder":"Root","Object":"<password_object>"}
 headers = {'content-type': 'application/json'}
